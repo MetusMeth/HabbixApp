cac37addcd0f5c19b6ad9b68d62580a79fc6acc4
Administrator <ryanleetweb@gmail.com>
2021-01-01 06:11:57 +0100

    Init 1.0.3

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const {app, BrowserWindow, Menu, ipcMain} = require('electron');
+const {app, BrowserWindow, Menu, ipcMain, globalShortcut} = require('electron');
 const {autoUpdater} = require("electron-updater");
 
 const path = require('path');
+const package = require('./package');
 const url = require('url');
 
 let pluginName;
 let pluginVersion;
 let mainWindow;
+
 switch (process.platform) {
     case 'win32':
         if (process.arch === "x32" || process.arch === "ia32") {
             pluginName = 'pepflashplayer-32.dll';
-            pluginVersion = '32.0.0.465';
+            pluginVersion = '32.0.0.363';
         } else {
             pluginName = 'pepflashplayer.dll';
-            pluginVersion = '20.0.0.306';
+            pluginVersion = '32.0.0.363';
         }
         break;
 }
+
 app.commandLine.appendSwitch("disable-renderer-backgrounding");
 app.commandLine.appendSwitch('high-dpi-support', "1");
 app.commandLine.appendSwitch('force-device-scale-factor', "1");
-
+app.commandLine.appendSwitch("--enable-npapi");
 app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname.includes(".asar") ? process.resourcesPath : __dirname, "flash/" + pluginName));
 
 Menu.setApplicationMenu(null)
@@ -29,9 +32,10 @@ Menu.setApplicationMenu(null)
 let sendWindow = (identifier, message) => {
     mainWindow.webContents.send(identifier, message);
 };
+
 let createWindow = async () => {
     mainWindow = new BrowserWindow({
-        title: "Habbix",
+        title: package.productName,
         icon: path.join(__dirname, '/icon.ico'),
         webPreferences: {
             plugins: true,
@@ -41,6 +45,7 @@ let createWindow = async () => {
             webSecurity: false
         },
         show: false,
+        frame: true,
         backgroundColor: "#000",
     });
     mainWindow.maximize();
@@ -66,10 +71,33 @@ let createWindow = async () => {
         mainWindow.webContents.executeJavaScript('enterHotel();');
     });
 
-    sendWindow("version", app.getVersion());
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.on("new-window", function(event, url) {
+        if (url.indexOf("/facebook") === -1) {
+            return;
+        }
 
-    ipcMain.on('clearcache', async () => {
+        event.preventDefault();
+        mainWindow.webContents.executeJavaScript('enterFacebook();');
+    });
+
+    sendWindow("version", app.getVersion());
+
+    if (package.debug) {
+        mainWindow.webContents.openDevTools();
+    } else {
+        globalShortcut.register("CTRL+SHIFT+F10", () => {
+            mainWindow.webContents.openDevTools();
+        });
+    }
+
+    ipcMain.on('cache', async () => {
+        let session = mainWindow.webContents.session;
+        await session.clearCache();
+        app.relaunch();
+        app.exit();
+    });
+
+    ipcMain.on('webCache', async () => {
         let session = mainWindow.webContents.session;
         await session.clearCache();
         app.relaunch();
@@ -81,7 +109,6 @@ let createWindow = async () => {
             mainWindow.setFullScreen(false);
         else
             mainWindow.setFullScreen(true);
-
     });
 
     ipcMain.on('zoomOut', () => {
@@ -96,6 +123,62 @@ let createWindow = async () => {
         if (factor < 3) {
             mainWindow.webContents.setZoomFactor(factor + 0.01);
         }
+    });
+
+    ipcMain.on('reload', () => {
+        mainWindow.reload();
+    });
+
+    ipcMain.on('facebook', function (event, arg) {
+        var options = {
+            client_id: package.clientId,
+            scopes: 'email',
+            redirect_uri: 'https://www.facebook.com/connect/login_success.html'
+        };
+
+        var authWindow = new BrowserWindow({
+            width: 600,
+            height: 300,
+            show: false,
+            parent: mainWindow,
+            modal: true,
+            webPreferences: {
+                nodeIntegration: false
+            }
+        });
+
+        var facebookAuthURL = `https://www.facebook.com/v3.2/dialog/oauth?client_id=${options.client_id}&redirect_uri=${options.redirect_uri}&response_type=token,granted_scopes&scope=${options.scopes}&display=popup`;
+
+        authWindow.loadURL(facebookAuthURL);
+        authWindow.webContents.on('did-finish-load', function () {
+            authWindow.show();
+        });
+
+        var access_token, error;
+        var closedByUser = true;
+
+        var handleUrl = function (url) {
+            var raw_code = /access_token=([^&]*)/.exec(url) || null;
+            access_token = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
+            error = /\?error=(.+)$/.exec(url);
+
+            if (access_token || error) {
+                closedByUser = false;
+                mainWindow.webContents.executeJavaScript('redirectFacebook();');
+                authWindow.close();
+            }
+        }
+
+        authWindow.webContents.on('will-navigate', (event, url) => handleUrl(url));
+        var filter = {
+            urls: [options.redirect_uri + '*']
+        };
+        mainWindow.webContents.session.webRequest.onCompleted(filter, (details) => {
+            var url = details.url;
+            handleUrl(url);
+        });
+
+        authWindow.on('close', () => event.returnValue = closedByUser ? { error: 'The popup window was closed' } : { access_token, error })
     });
 };
 
